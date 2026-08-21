dc05b15fc46f68c38e26fea415aaa5674e15931b
sasidhar5825-ux <sasidhar5825@gmail.com>
2026-08-21 11:54:38 +0530

    21082026

---
+++ python1.py
@@ -0,0 +1,23 @@
+""" x=[10,200,34,15,47]
+y=bytes(x)
+print(y[0])
+print(y[1])
+print(y[2])
+print(y[3])
+print(y[4]) """
+
+
+
+"""x=[10,255,34,15,47]
+y=bytes(x)
+for a in y:
+    print(a)
+"""
+
+a=range(5)
+print(a)
+for x in a:
+    print(x)
+
+
+
